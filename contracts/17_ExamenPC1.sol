5f33f2ded2d372dd0a1c8646a1008c2199694b15
BerrospiReyes <mauriccioreyes@gmail.com>
2026-05-16 19:58:49 -0500

    Parte 2

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -13,7 +13,19 @@ contract Grimorio260681 {
 
     Tomo[] public tomos;
 
+    address public dirContrato = 0xd9145CCE52D386f254917e481eB44e9943F39138;
+
     constructor() {
         console.log("Ejecutado por: 260681 - HERNAN MAURICCIO GASTON BERROSPI REYES");
     }
+
+    function agregarElemento(uint256 _id, string memory _titulo, string memory _autor) public {
+        tomos.push(Tomo(_id, _titulo, _autor));
+    }
+
+    function contarElementos() public view returns (uint256) {
+        console.log("Ejecutado por: 260681 - HERNAN MAURICCIO GASTON BERROSPI REYES");
+        return tomos.length;
+    }
+
 }
