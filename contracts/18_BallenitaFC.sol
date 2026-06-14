a33969ffeffe62680978b12e47959adbd62513fe
BerrospiReyes <mauriccioreyes@gmail.com>
2026-06-13 21:45:37 -0500

    subir clase 10

--- contracts/18_BallenitaFC.sol
+++ contracts/18_BallenitaFC.sol
@@ -1,10 +1,27 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.2 <0.9.0;
+pragma solidity >0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BallenitaFCToken is ERC20 {
+
+    address public owner;
+
     constructor() ERC20("Ballenita Futbol Club Token", "BFT") {
-        _mint(msg.sender, 1000);
+        owner = msg.sender;
+        _mint(owner, 1000 * 10 ** 10);
+    }
+
+    modifier isOwner() {
+        require(msg.sender == owner, "No eres el propietario");
+        _;
+    }
+
+    function mintear(address to, uint256 cantidad) public isOwner {
+        _mint(to, cantidad*10**10);
+    }
+
+    function esSocio(address cuenta) public view returns (bool) {
+        return balanceOf(cuenta) > 0;
     }
 }
